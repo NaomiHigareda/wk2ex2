75c22b1dbb5f45f28df60e7d170f75699a81081b
NaomiHigareda <nfhigareda@amarillocollege.com>
2025-01-26 19:46:34 -0600

    Sum of Digits

--- wk2ex2/Program.cs
+++ wk2ex2/Program.cs
@@ -18,14 +18,19 @@ namespace wk2ex2
             //prompt user to enter the sum of the of
             Console.WriteLine($"The sum of the digits {number} is {sum}");
         }
+        //method to calculate the sum
         static int SumOfDigits(int number)
         {   
             int sum = 0;
+            //loop until number is 0
             while (number != 0)
             {
+                //add the last digit to the sum
                 sum += number % 10;
+                //remove the last digit from the number
                 number /= 10;
             }
+            //return the calculated sum
             return sum;
         }
     }
