8d162c13a5e3e40c0cab5670697124fe3a7537d3
NaomiHigareda <nfhigareda@amarillocollege.com>
2025-01-26 18:22:48 -0600

    Add project files.

---
+++ wk2ex2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wk2ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt user to enter an integer
+            Console.WriteLine("Enter an integer:");
+            int number = int.Parse(Console.ReadLine());
+            int sum = SumOfDigits(number);
+
+            //prompt user to enter the sum of the of
+            Console.WriteLine($"The sum of the digits {number} is {sum}");
+        }
+        static int SumOfDigits(int number)
+        {   
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
